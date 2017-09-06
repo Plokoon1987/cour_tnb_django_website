4e5c3e1af9a620c23130acb7dbdbb933faa8b0c1
Froylan Málaga <jmalaga@idom.com>
2017-09-06 18:03:17 +0200

    Hasta el video 24 de Bucky incluido

--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
-from django.http import Http404
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 def index(request):
     all_albums = Album.objects.all()
@@ -9,13 +8,24 @@ def index(request):
 
 
 def detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album} )
+
+
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
     try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
-    return render(request, 'music/index.html', {'album': album} )
-
-
+        selected_song = album.song_set.get(pk=request.POST['song'])    
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html',{
+            'album': album,
+            'error_message': "You did not select a valid song"
+        }
+        )
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album} )
 
 
 # ***** Not Using a html Template *****
@@ -51,4 +61,12 @@ def detail(request, album_id):
 #    return HttpResponse("<h2>Details for Album ID:" + str(album_id) + "</h2>")
 
 
+# ***** Using try, except *****
+# Use: from django.http import Http404
 
+#def detail(request, album_id):
+#    try:
+#        album = Album.objects.get(pk=album_id)
+#    except Album.DoesNotExist:
+#        raise Http404("Album does not exist")
+#    return render(request, 'music/detail.html', {'album': album} )
